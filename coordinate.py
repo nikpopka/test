8b0439d586d4e9955f162ae383e7c8d1763041a3
Никита <n1k1ta.pop@yandex.ru>
2025-02-03 09:26:37 +0300

    git commit -m "тестовый файл2"

--- coordinate.py
+++ coordinate.py
@@ -3,9 +3,3 @@ import time
 import win32com.client
 shell = win32com.client.Dispatch("WScript.Shell")
 
-
-while True:
-    shell.SendKeys("W")
-    print('нажал')
-    time.sleep(2)
-    time.sleep(2)
